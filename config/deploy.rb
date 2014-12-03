f51ee85cce53c1e4e7208ba377522bd0eb23defd
Ben Rudolph <rudolphben@gmail.com>
2014-12-03 09:50:05 -0600

    Update cap deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require "capistrano/node-deploy"
 
-set :application, "unhcrdiction"
-set :repository,  "git@github.com:/benrudolph/chronicle.unhcrinnovation"
+set :application, "detention"
+set :repository,  "git@github.com:/benrudolph/detention.unhcrinnovation"
 set :scm, :git
 set :user, :deploy
 set :deploy_to, "/var/www/#{application}"
@@ -27,11 +27,4 @@ namespace :db do
     run "cp -f ~/unhcrdiction.json #{release_path}/config/config.json"
     run "cp -f ~/email.json #{release_path}/config/email.json"
   end
-
-  task :migrate, :except => { :no_release => true }, :role => :app do
-    run "cd #{release_path} && sequelize --migrate"
-  end
 end
-
-after "deploy:finalize_update", "db:config"
-
